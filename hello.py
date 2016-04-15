2dd1b9585e09ec3decce27843ccdd07a684220cb
Ubuntu <raulzrosa@gmail.com>
2016-04-15 16:05:28 -0300

    form

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello World</hi>"
+	return "<h1>Fill Out This Form</h1><form action='/hello' method='POST'>A Greeting: <input type='text' name='greet'> <br/>Your Name: <input type='text' name='name'><br/><input type='submit'>	</form>	"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
