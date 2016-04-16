7576c709807049228d52e453be45a37a7890b16e
Raul Zaninetti Rosa <raulzrosa@gmail.com>
2016-04-15 23:02:33 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
 import os
 from flask import Flask, request, render_template
-
+from math import exp
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<form action="/echo" method="POST"><input placeholder="peso(kg)" name="peso"><input placeholder="altura(m)" name="altura"><input type="submit" value="Echo"></form>'
+    return '<form action="/echo" method="POST"><input placeholder="peso(kg)" name="peso"><input placeholder="altura(m)" name="altura"><input type="submit" value="Calcular"></form>'
  
 @app.route("/echo", methods=['POST'])
 def echo(): 
-    return "You said: " + request.form.get('peso', '')
+    return "You said: " + request.form.get('peso', '') + float(request.form.get('peso', '')) / math.exp(float(request.form.get('altura', '')))
  
 
 if __name__ == "__main__":
