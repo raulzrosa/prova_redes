67f0e58068caa3737490cad1c7093c9cf20caab0
Raul Zaninetti Rosa <raulzrosa@gmail.com>
2016-04-15 22:19:35 -0300

    Create hello.py
    sdfdsfs

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
 import os
-from flask import Flask, render_template
-
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p> oi</p>"
+    return '<form action="/echo" method="POST"><input name="text"><input type="submit" value="Echo"></form>'
+ 
+@app.route("/echo", methods=['POST'])
+def echo(): 
+    return "You said: " + request.form['text']
+ 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
